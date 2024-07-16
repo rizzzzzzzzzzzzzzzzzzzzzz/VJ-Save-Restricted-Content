6e0764c4c9aa8caf5f3a6e8c611a691a726252dd
rizzzzzzzzzzzzzzzzzzzzzz <rizzz7419@gmail.com>
2024-07-16 11:16:21 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7189956610:AAFoqc8ziAAXLT27o7aFSvaNZ6oZpZvDE4g")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7333257544:AAENDSrpGyBsj1eAL-UGxqRF_ji8Ag61gK0")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "21857983"))
+API_ID = int(os.environ.get("API_ID", "21775862"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "e469e84c943ce3b8b056eb6a296f2c67")
+API_HASH = os.environ.get("API_HASH", "3cf8696da93ae9df8bf71ab84d64a1be")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://dhimanrajat:Y8IAGI0lVrMhjvkU@cluster0.mytkgu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Neha:Lm8qX34aUnOv23cb@cluster1.26t0hff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
